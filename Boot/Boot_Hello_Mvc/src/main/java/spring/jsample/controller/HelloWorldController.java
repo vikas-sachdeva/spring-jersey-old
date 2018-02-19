5beaac8adcce7bf9dbe33cfc581d4c69167648e0
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2018-02-19 10:54:40 +0900

    hello mvc spring boot

---
+++ Boot/Boot_Hello_Mvc/src/main/java/spring/jsample/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package spring.jsample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping(value = { "/", "/hello" })
+	public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "helloworld";
+
+	}
+}
